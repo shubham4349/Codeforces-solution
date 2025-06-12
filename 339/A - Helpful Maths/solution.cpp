df3c5c7f66366b34ff1a0158d6f755186893e077
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-06-12 09:07:00 +0530

    Add Helpful Maths [A] from Codeforces

---
+++ 339/A - Helpful Maths/solution.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+int main(){
+    
+    string s;
+    cin>>s;
+    
+    // split the string by +
+    vector<int> nums;
+    for(int i =0; i<s.length(); i++){
+        if(s[i]!='+'){
+            nums.pb(s[i]-'0');
+        }
+    }
+    sort(nums.begin(), nums.end());
+    
+    for(int i =0; i<nums.size(); i++){
+        
+        cout<<nums[i];
+        if(i== nums.size()-1) break;
+        cout<<"+";
+    }
+    
+    
+    
+   
+    
+    
+}
