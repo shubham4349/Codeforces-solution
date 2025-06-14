99e7aa2ca01996c325ca6fa0bc3eb058b3f98bc7
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-06-14 12:37:25 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main() {
+	
+ int a,b;
+ cin>>a>>b;
+ 
+ int rect_dimn= a*b;
+ int domino_dimn = 2*1; 
+ int ans = rect_dimn/ domino_dimn;
+ 
+ cout<<ans<<endl;
+ 
+	
+ 
+}
