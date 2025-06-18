4dc2810f45a9e80b690fba95485885b4428b5b58
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-06-18 08:30:57 +0530

    Add Game with Integers [A] from Codeforces

---
+++ 1899/A - Game with Integers/solution.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    
+    int t; cin>>t;
+    while(t--){
+         int n;cin>>n;
+    
+    cout<<(n%3==0 ? "Second" : "First")<<endl;
+    }
+    
+}
